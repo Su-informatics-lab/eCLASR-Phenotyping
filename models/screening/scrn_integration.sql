b5ac4bfea0d5665e980558b88a942e2e4fb8e046
Ryan Barnard <rbarnard@arane.us>
2019-09-24 18:12:17 -0400

    Added the wide criteria columns to the integration table.

--- models/screening/scrn_integration.sql
+++ models/screening/scrn_integration.sql
@@ -1,10 +1,18 @@
   WITH
+     patients AS (
+        SELECT DISTINCT patient_num FROM {{ ref('base_emr') }}
+     ),
+     cdg AS (
+        SELECT * FROM {{ ref('scrn_cdg_wide') }}
+     ),
      dist AS (
-        SELECT
-           patient_num,
-           {{ pivot('facility_id', get_column_values(ref('facilities'), 'facility_id'), agg='max', then_value='distance', else_value=0) }}
-          FROM {{ ref('scrn_distance') }}
-        GROUP BY patient_num
+        SELECT * FROM {{ ref('scrn_distance_wide') }}
      )
-SELECT *
-  FROM dist
+SELECT
+   pts.patient_num,
+   {{ star(from=ref('scrn_cdg_wide'), except=['patient_num']) }},
+   {{ star(from=ref('scrn_distance_wide'), except=['patient_num']) }}
+  FROM
+     patients pts
+        LEFT OUTER JOIN cdg ON (pts.patient_num=cdg.patient_num)
+        LEFT OUTER JOIN dist ON (cdg.patient_num=dist.patient_num)
