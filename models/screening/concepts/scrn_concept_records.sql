2d3b40649a4d43454e80c8af98fc012029076124
Ryan Barnard <rbarnard@arane.us>
2019-09-30 15:55:50 -0400

    Updated the list of concepts eligible for screening in the concepts module.

--- models/screening/concepts/scrn_concept_records.sql
+++ models/screening/concepts/scrn_concept_records.sql
@@ -1,4 +1,31 @@
-{% set screening_concepts = ['neuro', 'dep', 'bpd'] %}
+-- Screening concepts requiring just a simple match to a fact_concept code (e.g.
+-- ICD9, CPT, RXNORM, etc.)
+
+{%
+   set screening_concepts = [
+      'neuro',
+      'dep',
+      'bpd',
+      'schizo',
+      'sub_abuse',
+      'cardio',
+      'heart',
+      'stroke_large',
+      'tia',
+      'lung',
+      'renal',
+      'bone',
+      'therapy',
+      'bariatric',
+      'nursing_home',
+      'pregnant',
+      'clot',
+      'cancer',
+      'drugs_ad',
+      'drugs_psychoactive',
+      't2d_insulin'
+   ]
+%}
 
   WITH
      CDG AS (
