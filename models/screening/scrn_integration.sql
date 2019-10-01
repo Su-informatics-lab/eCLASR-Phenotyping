640c880fbd55f2dda5a26b4997ded5804daa7054
Ryan Barnard <rbarnard@arane.us>
2019-10-01 16:28:50 -0400

    Added vital sign exclusion fields into integration table.
    Fixed integration table join for distance table.

--- models/screening/scrn_integration.sql
+++ models/screening/scrn_integration.sql
@@ -5,14 +5,19 @@
      cdg AS (
         SELECT * FROM {{ ref('scrn_concepts_wide') }}
      ),
+     vitals AS (
+        SELECT * FROM {{ ref('scrn_vital_exclusions') }}
+     ),
      dist AS (
         SELECT * FROM {{ ref('scrn_distance_wide') }}
      )
 SELECT
    pts.patient_num,
    {{ star(from=ref('scrn_concepts_wide'), except=['patient_num']) }},
+   {{ star(from=ref('scrn_vital_exclusions'), except=['patient_num']) }},
    {{ star(from=ref('scrn_distance_wide'), except=['patient_num']) }}
   FROM
      patients pts
         LEFT OUTER JOIN cdg ON (pts.patient_num=cdg.patient_num)
-        LEFT OUTER JOIN dist ON (cdg.patient_num=dist.patient_num)
+        LEFT OUTER JOIN vitals ON (pts.patient_num=vitals.patient_num)
+        LEFT OUTER JOIN dist ON (pts.patient_num=dist.patient_num)
