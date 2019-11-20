2a67f2322ade567ed1a3905baac347c76515104d
Ryan Barnard <rbarnard@arane.us>
2019-11-20 15:23:50 -0500

    Added a stats table with the counts of overall, eligible, and ineligible.

---
+++ models/stats/stats_eligibility.sql
@@ -0,0 +1,14 @@
+    WITH
+        ec AS (
+            SELECT patient_num, eligible
+              FROM {{ ref('summary_patient_eligibility_conclusion') }}
+        ),
+        ptct AS (
+            SELECT
+                count(*)                                      AS num_patients,
+                sum(CASE WHEN eligible THEN 1 ELSE 0 END)     AS num_eligible,
+                sum(CASE WHEN NOT eligible THEN 1 ELSE 0 END) AS num_ineligible
+              FROM ec
+        )
+  SELECT *
+    FROM ptct
