6ad3952a702e02980f65be24a86c10e2ef3d56e6
Ryan Barnard <rbarnard@arane.us>
2019-11-20 15:05:56 -0500

    Added first-pass proof-of-concept table listing each patient's eligibility.

---
+++ models/stats/summary_patient_eligibility_evidence.sql
@@ -0,0 +1,42 @@
+-- TODO: Use the criteria table instead of hardcoding the fields to use.
+
+{%-
+   set criteria = {
+      'neuro': None,
+      'bpd': None,
+      'dep': '2018-04-01',
+   }
+-%}
+
+{%- set bmi_year = '5yrs' -%}
+{%- set bmi_col =  'bmi_{}'.format(bmi_year) %}
+
+{%- set vit_year = '5yrs' -%}
+{%- set vit_col =  'vit_{}'.format(vit_year) %}
+
+{%- set flds = criteria.keys()|list + [bmi_col, vit_col] -%}
+
+    WITH
+        scrn AS (
+            SELECT
+                patient_num,
+                {{ flds|join(', ') }}
+              FROM
+                  {{ ref('scrn_integration') }}
+        )
+        ,
+        eligible AS (
+            SELECT
+                patient_num,
+                {% for field, value in criteria.items() %}
+                   {{ field }} IS NULL
+                   {% if value %}
+                   OR {{ field }} < '{{ value }}'
+                   {% endif %} AS eligible_{{ field }},
+                {% endfor %}
+                coalesce({{ vit_col }}, FALSE) <> TRUE as eligible_vit,
+                coalesce({{ bmi_col }}, FALSE) <> TRUE as eligible_bmi
+              FROM scrn
+        )
+  SELECT *
+    FROM eligible
