852d129d90d60f5c588b07acae3740adad575215
Ryan Barnard <rbarnard@arane.us>
2019-11-20 16:15:41 -0500

    Added an eligibility table derived from the CDG instead of manually-specified fields and cutoffs.

---
+++ models/eligibility/eligibility_criteria_evidence.sql
@@ -0,0 +1,41 @@
+    WITH
+        cpts AS (
+            SELECT DISTINCT criteria_id, patient_num, latest_date
+              FROM {{ ref('scrn_concept_records_latest_criteria') }}
+        ),
+        cdg AS (
+            SELECT DISTINCT criteria_id, criteria_label, criteria_cutoff_date
+              FROM {{ ref('cdg_wide') }}
+        ),
+        ecrit AS (
+            SELECT
+                patient_num,
+                cdg.criteria_id,
+                criteria_label,
+                CASE
+                    WHEN cpts.latest_date IS NULL                    THEN TRUE
+                    WHEN cpts.latest_date < cdg.criteria_cutoff_date THEN TRUE
+                                                                     ELSE FALSE
+                END AS eligible,
+                cpts.latest_date,
+                cdg.criteria_cutoff_date
+              FROM
+                  cpts
+                      LEFT OUTER JOIN cdg ON (cpts.criteria_id = cdg.criteria_id)
+        ),
+        ecrit_wide AS (
+            SELECT
+                patient_num,
+                {{
+                   pivot('criteria_label',
+                      get_column_values(ref('scrn_concept_records_latest_criteria'), 'criteria_label', default=[]),
+                      agg='bool_and', then_value='eligible', else_value='TRUE'
+                   )
+                }}
+              FROM ecrit
+             GROUP BY patient_num
+        )
+  SELECT *
+    FROM ecrit_wide
+   ORDER BY
+       patient_num
