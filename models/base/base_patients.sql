3e95d486ca52fe3811b2125d2b0ec77b30dd5f13
Ryan Barnard <rbarnard@arane.us>
2020-01-02 15:00:15 -0500

    The base patients list now only includes patients that are present in both the EMR and demographics tables.

--- models/base/base_patients.sql
+++ models/base/base_patients.sql
@@ -1,1 +1,19 @@
-SELECT DISTINCT patient_num FROM {{ source('emr', 'emr') }}
+-- Only include patients that are present in both the EMR facts table
+-- and the demographics table. Patients lacking either demographic or
+-- fact records end up being excluded in downstream processing, and the
+-- absence of data often causes interpretation obstacles: better
+-- to exclude at the outset.
+
+WITH
+   emrPts AS (
+      SELECT DISTINCT patient_num FROM {{ source('emr', 'emr') }}
+   ),
+   demoPts AS (
+      SELECT DISTINCT patient_num FROM {{ source('emr', 'demo') }}
+   ),
+   pts AS (
+      SELECT * FROM emrPts
+         INTERSECT
+      SELECT * FROM demoPts
+   )
+SELECT patient_num FROM pts
