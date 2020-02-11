4ede861b23e16599afe543dde1fef100b7736ed8
Ryan Barnard <rbarnard@arane.us>
2020-02-11 16:12:57 -0500

    The "tall" distance model now includes all components of the facility identifiers (type, site, branch, and id).

--- models/screening/distance/scrn_distance.sql
+++ models/screening/distance/scrn_distance.sql
@@ -1,6 +1,9 @@
   WITH
      fac AS (
         SELECT
+           facility_type,
+           facility_site,
+           facility_branch,
            facility_id,
            latitude,
            longitude
@@ -16,6 +19,9 @@
      dist AS (
         SELECT
            pts.patient_num,
+           fac.facility_type,
+           fac.facility_site,
+           fac.facility_branch,
            fac.facility_id,
            {{ haversine_distance("fac.latitude", "fac.longitude", "pts.latitude", "pts.longitude") }} AS distance
           FROM
